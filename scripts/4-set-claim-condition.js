d9b0eb8c4d17cc625d57aca745dc828cad77be60
elijahola <elijaho@wustl.edu>
2022-02-17 09:41:27 -0700

    at dashboarrd

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0xc247e676CB8428ed43F76246B8eA6dEF2bb1F6a2",
+);
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    // Specify conditions.
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 50_000,
+      maxQuantityPerTransaction: 1,
+    });
+    
+    
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log("✅ Successfully set claim condition on bundle drop:", bundleDrop.address);
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})()
